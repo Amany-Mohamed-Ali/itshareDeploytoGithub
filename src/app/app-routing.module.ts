0909a05d18f0dc17f809c82fe5222c0de15afdd6
Amany Elmorsy <Amany@Nitcotek.com>
2019-11-03 15:13:01 +0200

    all source code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { LoginComponent } from './login/login.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'products', component: ProductsComponent},
+  {path: 'orders', component: MyOrdersComponent},
+  {path: 'admin/products', component: ProductsComponent},
+  {path: 'admin/orders', component: AdminOrdersComponent},
+  {path: 'shopping-cart', component: ShoppingCartComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'check-out', component: CheckoutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
